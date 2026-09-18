54651814b076d6f7de11ac15a40b9fd2433de473
jpl <jpl@jpl.jpl>
2026-09-18 19:33:35 +0300

    Polish layout and building color palette

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css'
 import './booking.css'
+import './polish.css'
 import './icons.css'
 
 createApp(App).mount('#app')
